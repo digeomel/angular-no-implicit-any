bf0347caa2c41c15d07f50b3bf1ccdd58395d381
DiGeomel <3295889+digeomel@users.noreply.github.com>
2023-10-09 10:20:12 +0200

    include the Test1Module and Test2Module in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { Test1Module } from 'projects/test1/src/public-api';
+import { Test2Module } from 'projects/test2/src/public-api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule, Test1Module, Test2Module],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
